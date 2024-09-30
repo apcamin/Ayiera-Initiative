4d439df3210de45d861fd76741339f5492a09724
michaelmuthomi <lit911call@gmail.com>
2024-09-30 22:43:56 +0300

    refactor: Update sidebar trigger size in Header component
    This commit updates the sidebar trigger size in the Header component. The change was made to improve the user experience and make it easier for users to interact with the sidebar.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -15,7 +15,7 @@ const SideBarComponents = ({trigger}) => {
     return (
       <Sheet>
         <SheetTrigger>{trigger}</SheetTrigger>
-        <SheetContent side="left" className="w-1/5">
+        <SheetContent side="left" className="md:w-1/5">
           <div className='flex flex-col gap-10'>
             <Logo src={AILOGO} />
             <section className='flex flex-col gap-6'>
@@ -24,9 +24,9 @@ const SideBarComponents = ({trigger}) => {
                 <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M4 13h6a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1zm-1 7a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v4zm10 0a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-7a1 1 0 0 0-1-1h-6a1 1 0 0 0-1 1v7zm1-10h6a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1h-6a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1z"/></svg>
                 Dashboard
               </Link>
-              <Link to="/talents" className="flex gap-2">
+              <Link to="/users" className="flex gap-2">
                 <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M9.5 12c2.206 0 4-1.794 4-4s-1.794-4-4-4-4 1.794-4 4 1.794 4 4 4zm1.5 1H8c-3.309 0-6 2.691-6 6v1h15v-1c0-3.309-2.691-6-6-6z"/><path d="M16.604 11.048a5.67 5.67 0 0 0 .751-3.44c-.179-1.784-1.175-3.361-2.803-4.44l-1.105 1.666c1.119.742 1.8 1.799 1.918 2.974a3.693 3.693 0 0 1-1.072 2.986l-1.192 1.192 1.618.475C18.951 13.701 19 17.957 19 18h2c0-1.789-.956-5.285-4.396-6.952z"/></svg>
-                Talents
+                Users
               </Link>
             </section>
           </div>
--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -1,29 +1,89 @@
 import Header from '@/components/Header'
 import { createFileRoute } from '@tanstack/react-router'
 import * as React from "react"
-import {supabase} from "@/backend/client.js"
+import { supabase } from "@/backend/client.js";
+import {ChartComponent} from "@/components/ChartComponent"
+import { LineGraph } from "@/components/LineGraph";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
 
-export const Route = createFileRoute('/dashboard')({
+export const Route = createFileRoute("/dashboard")({
   component: () => <AdminDashboard />,
-})
+});
 
-const fetchAdminUser = async ()  => {
-    const { data, error } = await supabase.from("administrator").select("*");
-    if (error) {
-        console.error(error)
-        return;
-    }
-    console.log(data)
+function CardComponent({
+  title,
+  content,
+  footer,
+  badgeText,
+}: {
+  title: string;
+  content: string;
+  footer?: string;
+  badgeText?: string;
+}) {
+  return (
+    <Card className="shadow-none">
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+      </CardHeader>
+      <CardContent className="flex items-center gap-4">
+        <h1 className="text-4xl">{content}</h1>
+        {badgeText == "increase" ? (
+          <Badge className="bg-green-600">Increase ↗</Badge>
+        ) : (
+            <Badge className="bg-red-600">Decrease ↙</Badge>
+        )}
+      </CardContent>
+      <CardFooter>
+        <p className="text-zinc-400 py-[-6rem] text-sm">{footer}</p>
+      </CardFooter>
+    </Card>
+  );
 }
 
-fetchAdminUser()
-
 function AdminDashboard() {
-    return (
-        <main>
-            <section>
-                <Header />
-            </section>
-        </main>
-    )
+  return (
+    <main>
+      <section className="sticky top-0 bg-white">
+        <Header />
+      </section>
+      <section className="px-6 pt-6 flex flex-col gap-4">
+        <h1 className="text-xl font-bold tracking-tight text-gray-900">
+          Dashboard
+        </h1>
+        <div className="grid md:grid-cols-3 gap-6 overflow-scroll">
+          <CardComponent
+            title="Total Users"
+            content="12,000"
+            footer="A 10% net increase"
+          />
+          <CardComponent
+            title="Administrators"
+            content="2,000"
+            footer="A 10% net increase"
+          />
+          <CardComponent
+            title="Total Awards"
+            content="1,200"
+            footer="A 10% net increase"
+            badgeText="increase"
+          />
+        </div>
+        <div className='grid md:grid-cols-3 gap-6'>
+          <div className="col-span-2">
+            <ChartComponent />
+          </div>
+          <LineGraph/>
+        </div>
+      </section>
+    </main>
+  );
 }
